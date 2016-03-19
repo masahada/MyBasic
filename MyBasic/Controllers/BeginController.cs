6aa1bedc18932cbe64b2d092150a63aa4844bb8b
Masaru Hada <masaruhada0116@gmail.com>
2016-03-19 09:46:56 +0900

    first commit

---
+++ MyBasic/Controllers/BeginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBasic.Controllers
+{
+    public class BeginController : Controller
+    {
+        // GET: Begin
+        public ActionResult Index()
+        {
+            return Content("こんにちは、世界！");
+        }
+    }
+}
---
+++ MyBasic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBasic.Startup))]
+namespace MyBasic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
